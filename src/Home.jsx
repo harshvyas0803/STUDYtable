b1dc75b4b7261a5ade8379ec76d36bd0a9b112be
Harsh Vyas <harshvyas0803@gmail.com>
2025-03-07 05:06:05 +0530

    update section2

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import ApplyButton from "./components/apply-button/ApplyButton";
 import SignInLink from "./components/signin/SignInLink";
 import hero_img from "./assets/hero-img.svg";
+import v2 from "./assets/v2.svg"
 
 const Home = () => {
   return (
@@ -11,7 +12,8 @@ const Home = () => {
 
 
       <div className="parent-container">
-        <div className="hero-container">
+
+        <section className="hero-container">
           <div className="left-of-hero">
             <div className="hero-content">
               <h2 className="hero-title">
@@ -34,9 +36,29 @@ const Home = () => {
               <img src={hero_img} alt="Hero illustration" className="hero-img" />
             </div>
           </div>
+        </section>
+
+        <section className="practice-section">
+  <div className="left-of-section">
+    <h2 className="section-title">Unlimited Practice session</h2>
+    <p className="section-description">
+      For topics and subjects you go through in your school and coaching every day, we create instant practice session specially made for your days need. Our innovative AI algorithm crafts batches of 20 questions, each accompanied by instant feedback and solutions – it's like having a Home tutor 24x7.
+    </p>
+  </div>
+  <div className="right-of-section">
+    <img src={v2} alt="illustration" className="section-img" />
+  </div>
+
+
+</section>
+        <div className="section3">
+
+
+            <div className="left-of-section3"></div>
+            <div className="right-of-section3"></div>
+
+
         </div>
-        <div className="section1"></div>
-        <div className="section2"></div>
         <div className="section3"></div>
         <div className="section4"></div>
         <div className="section5"></div>
