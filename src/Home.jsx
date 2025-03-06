251026fe8b68a6fca580f8cb7eaacdf858e50e39
Harsh Vyas <harshvyas0803@gmail.com>
2025-03-07 03:15:28 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+  <>  <h1 className='title'>STUDYtable</h1>
+
+    <div className="parent_container">
+
+
+<div className="hero_container">
+
+<div className="left_of_hero"></div>
+<div className="right_of_hero"></div>
+
+
+
+
+
+
+</div>
+<div className="section1"></div>
+<div className="section2"></div>
+<div className="section3"></div>
+<div className="section4"></div>
+<div className="section5"></div>
+
+
+<footer></footer>
+
+
+
+
+
+
+
+
+    </div>
+
+    </>
+  )
+}
+
+export default Home
