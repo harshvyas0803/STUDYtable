fdac70c486a2943600c5f80f6c1ad67d93e2094c
Harsh Vyas <harshvyas0803@gmail.com>
2025-03-07 18:55:13 +0530

    v2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,34 +4,34 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <footer className="footer">
-    <div className="footer-container">
-      <div className="footer-left">
-        <h3>Studytable</h3>
-        <p className="copyright">© Copyright 2025 Studytable</p>
-      </div>
-      <div className="footer-center">
-        <a href="#">NOTICE BOARD</a>
-      </div>
-      <div className="footer-right">
-        <div className="socials">
-          <h4>SOCIALS</h4>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-instagram"></i> Instagram
-          </a>
-          <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-whatsapp"></i> What's App
-          </a>
+      <div className="footer-container">
+        <div className="footer-left">
+          <h3>Studytable</h3>
+          <p className="copyright">© Copyright 2025 Studytable</p>
         </div>
-        <div className="blogs">
-          <h4>BLOGS</h4>
-          <a href="/how-we-plan-learning-session">How we plan learning session?</a>
-          <a href="/how-we-plan-assessment-session">How we plan assessment session?</a>
-          <a href="/how-we-manage-student-life">How we manage student life?</a>
-          <a href="/the-effective-student">The effective student</a>
+        <div className="footer-center">
+          <a href="#">NOTICE BOARD</a>
+        </div>
+        <div className="footer-right">
+          <div className="socials">
+            <h4>SOCIALS</h4>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram"></i> Instagram
+            </a>
+            <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-whatsapp"></i> What's App
+            </a>
+          </div>
+          <div className="blogs">
+            <h4>BLOGS</h4>
+            <a href="/how-we-plan-learning-session">How we plan learning session?</a>
+            <a href="/how-we-plan-assessment-session">How we plan assessment session?</a>
+            <a href="/how-we-manage-student-life">How we manage student life?</a>
+            <a href="/the-effective-student">The effective student</a>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   )
 }
 
